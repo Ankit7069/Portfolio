b5a60db026f40819583f365da774e71c050ef8cb
Om Patole <145087363+OmPatole@users.noreply.github.com>
2025-12-02 00:47:05 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-// NOTE: For GitHub Pages, set `base` to `'/REPO_NAME/'` (including the slashes),
-// where REPO_NAME is the name of your GitHub repository.
-// Example: if your repo is `om-patole/portfolio`, set base: '/portfolio/'.
 export default defineConfig({
-  base: '/Portfolio', // TODO: change this if your repo name is different
+  base: '/Portfolio',
   plugins: [tailwindcss(), react()],
 })
